1f90d7237280790b74c3511e6dd83cd0d7c28c58
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-05-04 15:31:16 -0400

    re-styled

--- R/get_covid19_dataset.R
+++ R/get_covid19_dataset.R
@@ -12,9 +12,7 @@
 #' nytimes_states <- get_covid19_dataset("covid19nytimes_states")
 #'
 #' tail(nytimes_states)
-#'
-
-get_covid19_dataset <- function(dataset){
+get_covid19_dataset <- function(dataset) {
   url <- paste0(repo_url, data_loc, dataset, ".csv")
 
   readr::read_csv(url)
