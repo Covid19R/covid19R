0271951635185130cb08414797a1fd24dddcd139
Jarrett Byrnes <jebyrnes@Jarretts-MacBook-Air-2.local>
2020-05-04 15:26:12 -0400

    Update globals to match new data repo

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 #where are all of the data files
 repo_url <- "https://github.com/Covid19R/covid19Rdata/raw/master/"
-data_loc <- "data/"
+data_loc <- "data-raw/"
 vocab_loc <- "controlled_vocabularies/"
